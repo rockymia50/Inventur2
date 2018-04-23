1b3c3f1bb98a1c98083ed875ff5c1cd5c61978bb
Roger McIntosh <rogermcintosh@Rogers-MacBook-Air.local>
2018-04-23 13:13:37 -0400

    update

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ import SourceMapSupport from 'source-map-support';
 const cors = require('cors')
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 
 // import routes
