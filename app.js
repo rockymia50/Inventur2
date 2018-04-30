4aa762e924ac52a4ade02c224e6ef1df47c03349
Fola <folaakingbe@yahoo.com>
2018-04-30 12:47:25 -0400

    deploy test

--- app.js
+++ app.js
@@ -53,11 +53,13 @@ SourceMapSupport.install();
 // set the port
 const port = process.env.PORT || 3001;
 
-// connect to database
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Inventur', {
- 
-});
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Inventur";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 app.use('/api', itemRoutes);
 app.get('/', (req,res) => {
   return res.end('Api working');
